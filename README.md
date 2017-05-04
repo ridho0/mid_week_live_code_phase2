d44cbe997e30612ee433390fdd5d57346a46498e
ridho0 <ridho230994@gmail.com>
2017-05-04 11:59:31 +0700

    udah test crud foods dan restaurant

--- README.md
+++ README.md
@@ -10,6 +10,17 @@ plus fitur login
 /api/signup|POST|Sign up with new user info
 /api/signin|POST|Sign in while get an access token based on credentials
 
+api/foods | get | foodController.getAllData
+api/foods/:id | get | foodController.getOneData //by ID
+api/foods | post | foodController.createData
+api/foods/:id | put | foodController.updateData //by ID
+api/foods/:id | delete | foodController.deleteData //by ID
+api/restaurants | get | restaurantController.getAllData
+api/restaurants/:id | get | restaurantController.getOneData //by ID
+api/restaurants | post | restaurantController.createData
+api/restaurants/:id | put | restaurantController.updateData //by ID
+api/restaurants/:id | delete | restaurantController.deleteData //by ID
+
 To Use
 ```javascript
 npm install
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 const Strategy = require('passport-local').Strategy;
 const passwordHash = require('password-hash');
 
-mongoose.connect('mongodb://localhost/mid_week_live_code_phase2')
+mongoose.connect('mongodb://localhost/library')
 
 passport.use(new Strategy(
   function(username, password, cb) {
@@ -25,15 +25,17 @@ passport.use(new Strategy(
 
 
 const app = express()
-
+app.set('port', process.env.PORT || 3000)
 app.use(require('morgan')('dev'))
 app.use(require('body-parser').urlencoded({ extended: false }))
 app.use(require('body-parser').json())
 
 app.use(passport.initialize())
-app.use('/users', require('./routes'));
+app.use('/', require('./routes'));
 // app.use('/', passport.authenticate('local', { session: false }), function(req, res) {
 //   res.send(req.user)
 // })
 
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Listening on port' + app.get('port'));
+})
